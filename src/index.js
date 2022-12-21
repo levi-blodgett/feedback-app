16f3af61c46fb6e85bcae056d852da1766a599ce
levi-blodgett <levi_blodgett@outlook.com>
2022-12-21 10:46:38 -0700

    Created first component, app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
+// React dependency
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+// Allows React to interact with DOM in browser
+import ReactDOM from 'react-dom';
+// Import App component
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Main app component to insert, and where to insert
+ReactDOM.render(<App />, document.getElementById('root'))
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
