73a72de24bc9e507aa1bf088ce72fa400611bd93
levi-blodgett <levi_blodgett@outlook.com>
2022-12-21 10:50:09 -0700

    init css, starting jsx

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@
 import React from 'react';
 // Allows React to interact with DOM in browser
 import ReactDOM from 'react-dom';
+// Import main css
+import './index.css';
 // Import App component
 import App from './App';
 
 // Main app component to insert, and where to insert
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    // Use strict mode for additional checks
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'))
 
