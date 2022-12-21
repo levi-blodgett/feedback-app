78277e81a3d6b57a5f364bfd20b1c2bab369b343
levi-blodgett <levi_blodgett@outlook.com>
2022-12-21 16:23:55 -0700

    Customized header css, filled out index.css file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,16 @@ import PropTypes from "prop-types";
 
 // Define header
 // Can use 'props' & props.text or { text } & {text}
-function Header({ text }) {
+function Header({ text, bgColor, textColor }) {
+  // Can use a var to edit header styles directly in jsx
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor,
+  };
+
   return (
-    <header>
+    // Add to header element: style={headerStyles}
+    <header style={headerStyles}>
       <div className="container">
         <h2>{text}</h2>
       </div>
@@ -20,12 +27,16 @@ function Header({ text }) {
 // Define default property value if not passed
 Header.defaultProps = {
   text: "Feedback UI",
+  bgColor: "rgba(0, 0, 0, 0.4)",
+  textColor: "#ff6a95",
 };
 
 // Define type of variables
-// Can add '.isRequired' to the end
+// Can add '.isRequired' to the end if you want it to always be defined
 Header.propTypes = {
   text: PropTypes.string.isRequired,
+  bgColor: PropTypes.string.isRequired,
+  textColor: PropTypes.string.isRequired,
 };
 
 export default Header;
